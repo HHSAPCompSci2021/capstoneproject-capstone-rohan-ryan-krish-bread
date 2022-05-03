90044df872418f11603230dd783a559457cdb6b3
Ryan Wagner <89160623+rwagner367@users.noreply.github.com>
2022-05-03 12:40:44 -0700

    added launch screens

---
+++ CapstoneProject/src/screenClasses/Launch1.java
@@ -0,0 +1,10 @@
+package screenClasses;
+
+public class Launch1 extends LaunchScreen{
+
+	public Launch1(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ CapstoneProject/src/screenClasses/Launch2.java
@@ -0,0 +1,10 @@
+package screenClasses;
+
+public class Launch2 extends LaunchScreen{
+
+	public Launch2(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
