b8fb00d2eb412d40d6749d626c4ead2353e824ef
Ryan Wagner <89160623+rwagner367@users.noreply.github.com>
2022-05-03 19:39:35 -0700

    launch screens added

---
+++ CapstoneProject/src/screenClasses/LaunchScreen.java
@@ -0,0 +1,10 @@
+package screenClasses;
+
+public class LaunchScreen extends Screen{
+
+	public LaunchScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- CapstoneProject/src/screenClasses/ScreenSwitcher.java
+++ CapstoneProject/src/screenClasses/ScreenSwitcher.java
@@ -4,8 +4,12 @@ package screenClasses;
 
 public interface ScreenSwitcher {
 	
-	public static final int BUILD_1 = 0;
-	public static final int BUILD_2 = 1;
+	public static final int LEVEL_SELECT = 0;
+	public static final int BUILD_1 = 1;
+	public static final int BUILD_2 = 2;
+	public static final int LAUNCH_1 = 3;
+	public static final int LAUNCH_2 = 4;
+
 	
 	public void switchScreen(int i);
 }
