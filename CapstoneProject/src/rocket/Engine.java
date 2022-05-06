7fa985e2776c6002219c0c266c8993517347e3d9
Krish Jhurani <89160597+KrishJhurani@users.noreply.github.com>
2022-05-06 12:34:04 -0700

    Part of Working Alpha Release

--- CapstoneProject/src/rocket/Engine.java
+++ CapstoneProject/src/rocket/Engine.java
@@ -2,22 +2,32 @@ package rocket;
 import java.util.List;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 import screenClasses.Sidebar;
 
 // Represents engines. Rocket class will make engine objects 
 public class Engine implements Sidebar {
 	private List<Engine> engine;
-	public Engine() {
+	private PImage image;
+	private double x, y, width, height;
+	public Engine(PImage image) {
+		this.image = image;
 		// TODO Auto-generated constructor stub
 	}
 	public List<Engine> getEngine() {
 		return engine;
 	}
+	public PImage getImage() {
+		return image;
+	}
 	@Override
-	public void draw(PApplet p) {
-		// use images
-
-		
+	public void draw(PApplet g) {
+		if (image != null)
+			g.image(image,(float)x,(float)y,(float)width,(float)height);
+		else {
+			g.fill(100);
+			g.rect((float)x,(float)y,(float)width,(float)height);
+		}
 	}
 
 }
