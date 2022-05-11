f8abdf109c5f24ba4286f9cff3eb0633569e4119
Krish Jhurani <89160597+KrishJhurani@users.noreply.github.com>
2022-05-11 13:10:55 -0700

    Explode

---
+++ CapstoneProject/src/rocket/Meteors.java
@@ -0,0 +1,38 @@
+package rocket;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Meteors {
+	private PImage img;
+	private double x, y;
+	public Meteors(PImage img, double x, double y) {
+		this.img = img;
+		this.x = x;
+		this.y = y;
+	}
+	public PImage img() {
+		return img;
+	}
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setMeteorY(double yVal) {
+		this.y = yVal;
+	}
+	public void draw(PApplet drawer) {
+		
+		if (img != null)
+			drawer.image(img,(float)x,(float)y,(float)50,(float)50);
+		else {
+			drawer.fill(100);
+			drawer.rect((float)x,(float)y,(float)50,(float)50);
+		}
+		
+		// if material not null, then draw it
+		
+	}
+}
--- CapstoneProject/src/screenClasses/Launch1.java
+++ CapstoneProject/src/screenClasses/Launch1.java
@@ -5,6 +5,8 @@ import java.awt.Rectangle;
 
 import buildClasses.BuildScreen;
 import main.DrawingSurface;
+import processing.core.PImage;
+import rocket.Meteors;
 import rocket.Rocket;
 
 public class Launch1 extends LaunchScreen{
@@ -12,30 +14,33 @@ public class Launch1 extends LaunchScreen{
 	private DrawingSurface surface;
 	private Rectangle button;
 	private Rocket rocket;
-	private double x, y;
+	private Meteors meteor;
+	private double rocketX, rocketY;
+	private double meteorX, meteorY;
 	private boolean start;
+	private int count;
 	public Launch1(DrawingSurface surface) {
 		super(800,600,surface);
 		this.surface = surface;
-		this.x = 300;
-		
-		this.y = 200;
+		this.rocketX= 300;
+		this.rocketY = 200;
+		this.meteorX = 300;
+		this.meteorY = 100;
 		button = new Rectangle(800/2-100,600/2-50,200,100);
 		start = false;
+		count = 0;
 	}
 	public void spawnRocket() {
 		//	img = surface.loadImage("img/rocket.png");
-			rocket = new Rocket(surface.loadImage("img/rocket.png"), this.x, this.y);
+			rocket = new Rocket(surface.loadImage("img/rocket.png"), this.rocketX, this.rocketY);
 	}
-	public void translateUp() {
-		y+=2;
-//		do {
-//			translateUp();
-//		} while() // Collisions with meteors
+	public void spawnMeteors() {
+		meteor = new Meteors(surface.loadImage("img/download.png"), this.meteorX, this.meteorY);
 	}
 
 	public void setup() {
 		spawnRocket();
+		spawnMeteors();
 	}
 	public void draw() {
 		
@@ -52,9 +57,20 @@ public class Launch1 extends LaunchScreen{
 		surface.text(str, button.x+button.width/2-w/2, button.y+button.height/2);
 		surface.fill(255);
 		rocket.draw(surface);
+		meteor.draw(surface);
+		
 //		surface.image(rocket.getImage(), (float)this.x, (float)this.y+6);
-		if (start)
-		rocket.setImageY(rocket.getY()-5);
+		if (start) {
+			rocket.setImageY(rocket.getY()-5);
+			meteor.setMeteorY(meteor.getY()+5);
+			if(touchMeteor(meteor.getY(), rocket.getY())) {
+				start = false;
+				count++;
+				surface.loadImage("img/giphy.gif");
+				surface.image(surface.loadImage("img/giphy.gif"),(float) rocket.getX(),(float) rocket.getY());
+			}
+		}
+		
 	
 	}
 	
@@ -70,19 +86,36 @@ public class Launch1 extends LaunchScreen{
 ////		set();
 //	}
 	public void keyPressed() {
-			start = true;
+		if(count == 1 && !start) {
+			return;
 		}
-		
-	
-//		if (keyCode == DOWN) {
-//			movingRectangle.moveBy(0, 4);
-//		}
+		else {
+			start = true;
+			 if (surface.keyCode== surface.LEFT) {
+			       rocketX= rocketX-20;
+			     }
+			 if (surface.keyCode==surface.RIGHT) {
+			      rocketX = rocketX+20;
+			    }
+		}
 
+//			if(!start) {
+//				return;
+//			}
+			    
+			
+		}
+	public boolean touchMeteor(double yMeteor, double yRocket) {
+		   if (yMeteor==yRocket) { 
+			   return true;
+		 }
+		return false;
+	}
+	
 	public void moveBy(double amountX, double amountY) {
 		double x1 = rocket.getX();
 		double y1 = rocket.getY();
 		this.rocket.setImageX(x1+=amountX);
 		this.rocket.setImageY(y1+=amountY);
 	}
-
 }
