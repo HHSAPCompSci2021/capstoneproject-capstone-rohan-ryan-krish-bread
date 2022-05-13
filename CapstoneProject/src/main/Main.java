da8aa5cf47fadbe2dd02c00da1230b31840c06ae
Krish Jhurani <89160597+KrishJhurani@users.noreply.github.com>
2022-05-13 12:28:09 -0700

    PUSH

--- CapstoneProject/src/main/Main.java
+++ CapstoneProject/src/main/Main.java
@@ -6,7 +6,6 @@ import javax.swing.JFrame;
 import processing.awt.PSurfaceAWT;
 import processing.core.PApplet;
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DrawingSurface drawing = new DrawingSurface();
