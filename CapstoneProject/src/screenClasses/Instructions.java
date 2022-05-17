5fcb4f7e0dbaf3b2900a06f2663ecc8d136807d4
Ryan Wagner <89160623+rwagner367@users.noreply.github.com>
2022-05-17 11:15:23 -0700

    instructions

---
+++ CapstoneProject/src/screenClasses/Instructions.java
@@ -0,0 +1,38 @@
+package screenClasses;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+
+import main.DrawingSurface;
+
+public class Instructions extends Screen{
+
+	private DrawingSurface surface;
+	private Rectangle button;
+	
+	public Instructions(int width, int height, DrawingSurface surface) {
+		super(width, height);
+		this.surface = surface;
+		
+		button = new Rectangle(425,550,150,30);
+	}
+	
+	public void draw() {
+		surface.rect(button.x, button.y, button.width, button.height, 10, 10, 10, 10);
+		surface.fill(0);
+		String str = "Back To Level Select";
+		float w = surface.textWidth(str);
+		surface.text(str, button.x+button.width/2-w/2, button.y+button.height/2);
+		surface.fill(255);
+	}
+	
+	/**
+	 * detects where the mouse was pressed, and activates the button if it was pressed
+	 */
+	public void mousePressed() {
+		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX,surface.mouseY));
+		if (button.contains(p)) {
+			surface.switchScreen(ScreenSwitcher.LEVEL_SELECT);
+		}			
+	}
+}
