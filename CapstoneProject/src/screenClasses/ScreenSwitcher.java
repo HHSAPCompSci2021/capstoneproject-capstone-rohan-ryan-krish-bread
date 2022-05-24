27c0680fed7e28701e35770767dcd6714d64d097
Krish Jhurani <89160597+KrishJhurani@users.noreply.github.com>
2022-05-23 19:08:46 -0700

    PSUSH

--- CapstoneProject/src/screenClasses/ScreenSwitcher.java
+++ CapstoneProject/src/screenClasses/ScreenSwitcher.java
@@ -11,9 +11,7 @@ public interface ScreenSwitcher {
 	public static final int LAUNCH_2 = 4;
 	public static final int INSTRUCTIONS = 5;
 	public static final int LAUNCH_3 = 6;
-	public static final int LAUNCH_4 = 7;
-	public static final int BUILD_3 = 8;
-	public static final int BUILD_4 = 9;
+	public static final int BUILD_3 = 7;
 
 	
 	public void switchScreen(int i);
