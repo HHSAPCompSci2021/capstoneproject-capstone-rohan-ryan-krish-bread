31d585d777b5cd86ca7f9f45ab5eab9e9447840c
Ryan Wagner <89160623+rwagner367@users.noreply.github.com>
2022-05-24 10:55:53 -0700

    Update ScreenSwitcher.java

--- CapstoneProject/src/screenClasses/ScreenSwitcher.java
+++ CapstoneProject/src/screenClasses/ScreenSwitcher.java
@@ -1,7 +1,9 @@
 package screenClasses;
 
-
-
+/**
+ * Represents the screens that can be switched to
+ * @author Ryan, Rohan, Krish
+ */
 public interface ScreenSwitcher {
 	
 	public static final int LEVEL_SELECT = 0;
@@ -13,6 +15,9 @@ public interface ScreenSwitcher {
 	public static final int LAUNCH_3 = 6;
 	public static final int BUILD_3 = 7;
 
-	
+	/**
+	 * Switches the screen that is being drawn
+	 * @param i Integer that is used to specify the new screen
+	 */
 	public void switchScreen(int i);
 }
